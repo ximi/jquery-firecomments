d1d2d56467a0d0ee0fdb173e6a2aeac35ee0daf5
Maximilian Zimmer <ximi@Maxintosh.local>
2014-04-06 22:59:01 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,98 @@
-jquery-firecomments
-===================
+# jQuery FireComments #
 
-Use Firebase to add a basic commenting system to any page
+## Add a commenting system powered by Firebase to any page ##
+
+### What is jQuery FireComments? ###
+This is the alpha release of jQuery FireComments, a [jQuery](http://jquery.com/) plugin that let's you add a simple commenting system to any page without the need for your own backend by using [Firebase](https://www.firebase.com/) to store the comment data.
+
+### Why should I use this? ###
+ * Because you want to let users comment on a static website without a backend or a system that doesn't have a built-in commenting system
+ * Because you don't want to rely on third party commenting systems such as Facebook Comments, disqus, IntenseDebate (Even though your comments are hosted on Firebase, you are the one that controls the data. It is similar to store them in any hosted sql database.)
+ * Because you want comments to show up on your page in real time to encourage conversations
+
+### How do I use it? ###
+ 1. Sign up for a free [Firebase Account](https://www.firebase.com/signup/). Their free plan should suffice for all smaller websites (around 70000 monthly pageviews according to Firebase). If you have a website with a large amount of traffic you will need to sign up for one of their paid plans.
+ 2. Add security rules to your Firebase. If you aren't going to modify the default url structure (as defined by the parent_path/child_path options) you can use the rules from the firebase_security_rules_example.json or adjust them according to your needs.
+ 3. Include jquery, firebase and firecomments on your page
+         <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+         <script src="https://cdn.firebase.com/v0/firebase.js"></script>
+         <script src="jquery.firecomments.js"></script>
+ 4. Call jQuery FireComments on the element you want the comments to appear in with your firebase url:
+        $("#element").fireComments({
+            firebase_url: 'https://sizzling-fire-1457.firebaseio.com/'
+        });
+
+### Options ###
+
+#### firebase_url ####
+Default: undefined
+Type: url-escaped string
+This is the only option that is required. It's the url to your firebase. If you run into any problems with Firebase, check out their excellent [docs](https://www.firebase.com/docs/web-quickstart.html)
+
+#### id ####
+Default: '1'
+Type: url-escaped string
+This uniquely identifies the page you are adding and retrieving comments for. If you are only ever going to add comments to a single page you do not need to provide it. If you want to add FireComments to several pages, you'll need to pass a unique, url-escaped value into each
+
+#### parent_path ####
+Default: 'posts'
+Type: url-escaped string
+This is the parent container individual pages (identified by the id) are stored under in your Firebase.
+
+#### child_path ####
+Default: 'comments'
+Type: url-escaped string
+This is the container your comments are stored under in your Firebase. This is useful if you also store additional information about your pages/posts in the same firebase.
+
+#### new_desc ####
+Default: true
+Type: boolean
+Setting this value to true will order comments from newest to oldest. Setting it to false will show the oldest comment first and the newest last
+
+#### comments_container ####
+Default: '<section id="fire-comments"></section>'
+Type: html string | jQuery object
+This is the html container comments will be added to. It can be a jQuery object that references an existing element on the page, a new jQuery object or a string representing the desired html.
+
+#### form ####
+Default: '<form id="fire-comment-form"></form>',
+Type: html string | jQuery object
+This is the form element the comment form will be added to. It can be a jQuery object that references an existing element on the page, a new jQuery object or a string representing the desired html.
+
+If you are passing an existing form that already contains input elements, FireComments presumes you are providing the comment inputs and no longer generates them. In that case make sure that the inputs have the right ids (`fire-comment-name`, `fire-comment-website`, `fire-comment-content`).
+
+#### form_before ####
+Default: true
+Type: boolean
+If set to true the generated form will be added before the comments, if set to false after. If you are providing your own form, this option is ignored.
+
+#### renderComment ####
+Default: undefined
+Type: function
+Arguments: snapshot
+This let's you render comments yourself. This is called whenever a new comment is added. The snapshot argument has 3 properties, name, website and content. You can either return a html string or directly manipulate the dom yourself and return false/nothing.
+
+#### renderForm ####
+Default: undefined
+Type: function
+This let's you render the comment form yourself. You can either return a html string or directly manipulate the dom yourself and return false/nothing.
+
+#### renderFormGroup ####
+Default: undefined
+Type: function
+Arguments: label, slug, type, required
+This let's you render the form inputs and its related html yourself. You can either return a html string or directly manipulate the dom yourself and return false/nothing.
+
+### Plans for the Future ###
+#### Short Term ####
+ * Add the ability to define custom fields
+ * Basic spam proteciton
+ * Ability to edit/delete comment while still on page
+ * Better, more detailed readme/docs
+ * much, much more...
+
+#### Long Term ####
+ * Ability to remember user (prefill fields, delete/edit old comments)
+ * Better way to customize html structure (basic templating system)
+ * jQuery independent version
+ * much, much more
---
+++ dist/jquery.firecomments.js
@@ -0,0 +1,234 @@
+/*
+ *  jQuery FireComments - v0.1.0
+ *  Add a commenting system powered by Firebase to any page
+ *  https://github.com/ximi/jquery-firecomments/
+ *
+ *  Made by Max (ximi)
+ *  Under MIT License
+ */
+;(function($, window, document, undefined) {
+    'use strict';
+
+    // Set up our defaults
+    var defaults = {
+            id: '1',
+            parent_path: 'posts',
+            child_path: 'comments',
+            new_desc: true,
+            comments_container: '<section id="fire-comments"></section>',
+            form: '<form id="fire-comment-form"></form>',
+            form_before: true
+        },
+        utilities = {
+            trimSlashes: function(string) {
+                string = string.replace(/^\/+/, '');
+                string = string.replace(/\/+$/, '');
+                return string;
+            },
+            nl2br: function(str) {
+                return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '<br />' + '$2');
+            },
+            stripHtml: function(html) {
+                return $('<div></div>').html(html).text();
+            },
+            isInDom: function(element) {
+                return $.contains(document.documentElement, $(element)[0]);
+            }
+        };
+
+    function FireComments (element, options) {
+        this.element = element;
+        this.$element = $(element);
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = 'firecomments';
+        this.init();
+    }
+
+    FireComments.prototype = {
+        init: function() {
+            // store a reference to our comment container
+            this.$comments_container = $(this.settings.comments_container);
+            // if it isn't in the dom already, append it to the element
+            if(!utilities.isInDom(this.$comments_container)) {
+                this.$comments_container.appendTo(this.$element);
+            }
+
+            // store a reference to our form
+            this.$form = $(this.settings.form);
+            // if the form isn't already in the dom, let's add it
+            if(!utilities.isInDom(this.$form)) {
+                var method = this.settings.form_before ? 'prependTo' : 'appendTo';
+                this.$form[method](this.$element);
+            }
+
+            // let's render our form
+            this.renderForm();
+
+            // connect to Firebase
+            this.connect();
+
+            // setup our events
+            this.setupEvents();
+        },
+        connect: function() {
+            // make sure we have a firebase url
+            if(typeof this.settings.firebase_url === 'undefined') {
+                throw new Error('No Firebase URL has been passed to fireComments');
+            }
+            else {
+                // let's connect
+                this.updateUrl();
+                try {
+                    this.firebase = new Firebase(this.url);
+                }
+                catch (e) {
+                    throw new Error('The following error occured while connecting to Firebase: ' +  e.message);
+                }
+            }
+        },
+        updateUrl: function() {
+            this.url = this.settings.firebase_url +
+                     (this.settings.parent_path ? '/' + utilities.trimSlashes(this.settings.parent_path) : '') +
+                     (this.settings.id ? '/' + this.settings.id : '') +
+                     (this.settings.child_path ? '/' + utilities.trimSlashes(this.settings.child_path) : '') +
+                     '/';
+        },
+        setupEvents: function() {
+            if(this.firebase) {
+                var that = this;
+                this.firebase.on('child_added', function(snapshot) {
+                    that.renderComment(snapshot);
+                });
+                this.$form.on('submit', function(e) {
+                    // prevent the form from actually submitting
+                    e.preventDefault();
+                    that.saveComment();
+                });
+            }
+        },
+        renderComment: function(snapshot) {
+            if(snapshot) {
+                var comment = snapshot.val(),
+                    rendered_comment;
+
+                if($.isFunction(this.settings.renderComment)) {
+                    rendered_comment = this.settings.renderComment.apply(this.element, [comment]);
+                }
+                else {
+                    var comment_html = '<article class="fire-comment">';
+                    comment_html += '<div class="fire-comment-meta">';
+                    if(comment.website) {
+                        comment_html += '<a href="' + comment.website + '" class="fire-commenter">' + comment.name + '</a>';
+                    }
+                    else {
+                        comment_html += '<span class="fire-commenter">' + comment.name + '</a>';
+                    }
+                    comment_html += '</div>';
+                    comment_html += '<div class="fire-comment-content">';
+                    comment_html += comment.content;
+                    comment_html += '</div>';
+                    comment_html += '</article>';
+
+                    rendered_comment = comment_html;
+                }
+
+                if(typeof rendered_comment !== 'undefined') {
+                    var method = this.settings.new_desc ? 'prepend' : 'append';
+                    this.$comments_container[method](rendered_comment);
+                }
+            }
+        },
+        renderForm: function() {
+            // use custom function for rendering the form if it has been passed as option
+            var rendered_form;
+            if($.isFunction(this.settings.renderForm)) {
+                rendered_form = this.settings.renderForm.apply(this.element);
+            }
+            else {
+                // check if our form is empty, if not we're presuming the comment fields are already present
+                if(!this.$form.find('input, textarea').length) {
+                    var form_html = this.renderFormGroup('Name', 'name', 'text');
+                    form_html += this.renderFormGroup('Website', 'website', 'url');
+                    form_html += this.renderFormGroup('Comment', 'content', 'textarea', true);
+                    form_html += '<input type="submit" value="Submit" />';
+                    rendered_form = form_html;
+                }
+            }
+
+            /*
+             * it's possible for the custom function not to return any value
+             * as the user has directly manipulated the dom
+             * or that the form fields already exists
+             * in either case we won't touch the dom anymore
+             */
+            if(typeof rendered_form !== 'undefined') {
+                this.$form.html(rendered_form);
+            }
+        },
+        renderFormGroup: function(label, slug, type, required) {
+            var rendered_form_group;
+
+            // use custom function for rendering form groups if it has been passed as option
+            if($.isFunction(this.settings.renderFormGroup)) {
+                rendered_form_group = this.settings.renderFormGroup.apply(this.element, [label, slug, type, required]);
+            }
+            else {
+                var form_group_html = '<div class="fire-comment-form-group">';
+                form_group_html += '<label class="fire-comment-form-label" for="fire-comment-' + slug + '">' + label + '</label>';
+                if(type === 'textarea') {
+                    form_group_html += '<textarea';
+                }
+                else {
+                    form_group_html += '<input type="' + type + '"';
+                }
+                form_group_html += ' name="fire-comment-' + slug + '" id="fire-comment-' + slug + '"';
+                if(required) {
+                    form_group_html += ' required="required"';
+                }
+                if(type === 'textarea') {
+                    form_group_html += '></textarea>';
+                }
+                else {
+                    form_group_html += ' />';
+                }
+                form_group_html += '</div>';
+                rendered_form_group = form_group_html;
+            }
+
+            /*
+             * it's possible for the custom function not to return any value
+             * as the user has directly manipulated the dom
+             * in which case we won't return a value either
+             */
+            if(typeof rendered_form_group !== 'undefined') {
+                return rendered_form_group;
+            }
+        },
+        saveComment: function() {
+            var comment_name = this.$form.find('#fire-comment-name').val(),
+                comment_website = this.$form.find('#fire-comment-website').val(),
+                comment_content = this.$form.find('#fire-comment-content').val();
+
+            // Some light data sanitation
+            comment_name = $.trim(utilities.stripHtml(comment_name));
+            comment_website = $.trim(utilities.stripHtml(comment_website));
+            comment_content = $.trim(utilities.nl2br(utilities.stripHtml(comment_content)));
+
+            this.firebase.push({
+                name: comment_name,
+                website: comment_website,
+                content: comment_content
+            });
+        }
+    };
+
+    $.fn.fireComments = function(options) {
+        this.each(function() {
+            if(!$.data(this, 'firecomments')) {
+                $.data(this, 'firecomments', new FireComments(this, options));
+            }
+        });
+        return this;
+    };
+})(jQuery, window, document);
